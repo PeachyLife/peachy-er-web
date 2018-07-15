f778456647334613c5aa47ceefc238b02644f96c
jthetzel <jthetzel@gmail.com>
2018-07-15 17:07:10 -0230

    Add styleguidist

---
+++ styleguide.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+  title: 'Peachy styleguide',
+  sections: [
+    {
+      name: 'UI Components',
+      components: 'src/components/*.js'
+    },
+  ],
+  skipComponentsWithoutExample: true
+}
