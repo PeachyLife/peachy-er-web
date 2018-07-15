dce8cda05321993cca14435b26d763fc52939a1a
jthetzel <jthetzel@gmail.com>
2018-07-15 18:26:48 -0230

    Add by line

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to 🍑 Peachy</h1>
+          <h1 className="App-title">🍑 Peachy</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          The Time is Ripe.
         </p>
       </div>
     );
